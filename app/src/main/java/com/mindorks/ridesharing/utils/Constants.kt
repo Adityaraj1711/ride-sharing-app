6c50eb693285299c4f747b3beb5d8cdfc36c4b00
Adityaraj1711 <adityamailbox0@gmail.com>
2020-09-02 01:15:23 +0530

    added pickup and drop location UI in the activity Main xml file

--- app/src/main/java/com/mindorks/ridesharing/utils/Constants.kt
+++ app/src/main/java/com/mindorks/ridesharing/utils/Constants.kt
@@ -4,6 +4,7 @@ object Constants{
     const val TYPE = "type"
     const val NEAR_BY_CABS = "nearByCabs"
     const val LOCATIONS = "locations"
+    const val REQUEST_CAB = "requestCab"
     const val LAT = "lat"
     const val LNG = "lng"
 }

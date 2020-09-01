19e8aa6e4bfca975eabbca585c8d0fea9d66955e
Adityaraj1711 <adityamailbox0@gmail.com>
2020-09-01 22:01:23 +0530

    permission request for location from gps or network

--- app/src/main/java/com/mindorks/ridesharing/ui/maps/MapsActivity.kt
+++ app/src/main/java/com/mindorks/ridesharing/ui/maps/MapsActivity.kt
@@ -1,18 +1,23 @@
 package com.mindorks.ridesharing.ui.maps
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.mindorks.ridesharing.R
 import com.mindorks.ridesharing.data.network.NetworkService
+import com.mindorks.ridesharing.utils.PermissionUtils
 import com.mindorks.ridesharing.utils.ViewUtils
 
 class MapsActivity : AppCompatActivity(), MapsView, OnMapReadyCallback {
 
     companion object {
         private const val TAG = "MapsActivity"
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 999
     }
     private lateinit var presenter: MapsPresenter
     private lateinit var mMap: GoogleMap
@@ -30,5 +35,54 @@ class MapsActivity : AppCompatActivity(), MapsView, OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+    }
+
+    override fun onStart() {
+        super.onStart()
+        when{
+            PermissionUtils.isAccessFineLocationGranted(this) -> {
+                when{
+                    PermissionUtils.isLocationEnabled(this) -> {
+                        // fetch the location
+
+                    }
+                    else ->{
+                        PermissionUtils.showGPSNotEnabledDialog(this)
+                    }
+                }
+            }
+            else -> {
+                PermissionUtils.requestAccessFineLocationPosition(this,
+                    LOCATION_PERMISSION_REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    when{
+                        PermissionUtils.isLocationEnabled(this) -> {
+                            // fetch the result
+                        }
+                        else -> {
+                            PermissionUtils.showGPSNotEnabledDialog(this)
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Location Permission not granted", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+    override fun onDestroy() {
+        presenter.onDetach()
+        super.onDestroy()
     }
 }
